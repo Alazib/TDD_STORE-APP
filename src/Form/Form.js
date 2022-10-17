7b1f5dda12f5e76b25fc2f340f39ddb8da53f184
J.Alberto Azevedo <alazib7@gmail.com>
2022-10-17 17:05:40 +0200

    Disabled submit button

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,7 +1,98 @@
-import React from "react"
+import React, { useState } from "react"
+import TextField from "@mui/material/TextField"
+import InputLabel from "@mui/material/InputLabel"
+import NativeSelect from "@mui/material/NativeSelect"
+import Button from "@mui/material/Button"
 
 function Form() {
-  return <h1>create product</h1>
+  const [isSaving, setIsSaving] = useState(false)
+  const [formErrors, setFormErrors] = useState({
+    name: "",
+    size: "",
+    type: "",
+  })
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    setIsSaving(true)
+
+    const { name, size, type } = e.target.elements
+
+    if (!name.value) {
+      setFormErrors((prevState) => ({
+        ...prevState,
+        name: "The name is required",
+      }))
+    }
+    if (!size.value) {
+      setFormErrors((prevState) => ({
+        ...prevState,
+        size: "The size is required",
+      }))
+    }
+    if (!type.value) {
+      setFormErrors((prevState) => ({
+        ...prevState,
+        type: "The type is required",
+      }))
+    }
+
+    await fetch("/products", {
+      method: "POST",
+      body: JSON.stringify({}),
+    })
+
+    setIsSaving(false)
+  }
+
+  function handleBlur(e) {
+    const { name, value } = e.target
+    setFormErrors({
+      ...formErrors,
+      [name]: value.length > 0 ? "" : `The ${name} is required`,
+    })
+  }
+
+  return (
+    <>
+      <h1>create product</h1>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="name"
+          id="name"
+          name="name"
+          helperText={formErrors.name}
+          onBlur={handleBlur}
+        ></TextField>
+        <TextField
+          label="size"
+          id="size"
+          name="size"
+          helperText={formErrors.size}
+          onBlur={handleBlur}
+        ></TextField>
+        <InputLabel htmlFor="type">type</InputLabel>
+        <NativeSelect
+          defaultValue={30}
+          inputProps={{
+            name: "",
+            id: "type",
+          }}
+        >
+          <option aria-label="None" value=""></option>
+          <option value="electronic">Electronic</option>
+          <option value="furniture">Furniture</option>
+          <option value="clothing">Clothing</option>
+        </NativeSelect>
+        {formErrors.type.length > 0 && <span>{formErrors.type}</span>}
+
+        <Button type="submit" disabled={isSaving}>
+          Submit
+        </Button>
+      </form>
+    </>
+  )
 }
 
 export default Form
--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -1,11 +1,98 @@
-import { screen, render } from "@testing-library/react"
+import { screen, render, fireEvent, waitFor } from "@testing-library/react"
 import Form from "./Form"
+import user from "@testing-library/user-event"
+import { rest } from "msw"
+import { setupServer } from "msw/node"
+
+function renderForm() {
+  render(<Form />)
+}
+const server = setupServer(
+  rest.get("/products", (req, res, ctx) => res(ctx.status(201)))
+)
+
+beforeAll(() => server.listen())
+
+afterAll(() => server.close())
 
 describe("when form is mounted", () => {
   it("there must be a create product form page", () => {
-    render(<Form />)
+    renderForm()
+    const formPage = screen.getByRole("heading", { name: /create product/i })
   })
-  expect(screen.getByText(/create product/)).toBeInTheDocument()
-  // eslint-disable-next-line testing-library/no-debugging-utils
-  screen.debug()
+
+  it("should exist the fields: name, size, type (electronic, furniture, clothing", () => {
+    renderForm()
+
+    const nameField = screen.getByRole("textbox", { name: /name/i })
+    const sizeField = screen.getByRole("textbox", { name: /size/i })
+    const typeField = screen.getByRole("combobox", { name: /type/i })
+
+    const electronicOption = screen.getByRole("option", { name: /electronic/i })
+    const furnitureOption = screen.getByRole("option", { name: /furniture/i })
+    const clothingOption = screen.getByRole("option", { name: /clothing/i })
+  })
+
+  it("should exist the submit button", () => {
+    renderForm()
+    const submitButton = screen.getByRole("button", { name: /submit/i })
+  })
+})
+
+describe("when the user submits the form without values", () => {
+  it("should display validation messages", () => {
+    renderForm()
+
+    const helperTextName = screen.queryByText(/the name is required/i)
+    const helperTextSize = screen.queryByText(/the size is required/i)
+    const helperTextType = screen.queryByText(/the type is required/i)
+
+    expect(helperTextName).not.toBeInTheDocument()
+    expect(helperTextSize).not.toBeInTheDocument()
+    expect(helperTextType).not.toBeInTheDocument()
+
+    const submitButton = screen.getByRole("button", { name: /submit/i })
+    user.click(submitButton)
+
+    expect(screen.getByText(/the name is required/i)).toBeInTheDocument()
+    expect(screen.getByText(/the size is required/i)).toBeInTheDocument()
+    expect(screen.getByText(/the type is required/i)).toBeInTheDocument()
+  })
+})
+
+describe("when the user blurs and empty field", () => {
+  it("should display a validation error message", () => {
+    renderForm()
+
+    const helperTextName = screen.queryByText(/the name is required/i)
+    const helperTextSize = screen.queryByText(/the size is required/i)
+
+    expect(helperTextName).not.toBeInTheDocument()
+    expect(helperTextSize).not.toBeInTheDocument()
+
+    const nameField = screen.getByRole("textbox", { name: /name/i })
+    const sizeField = screen.getByRole("textbox", { name: /size/i })
+
+    fireEvent.blur(nameField, { target: { name: "name", value: "" } })
+    fireEvent.blur(sizeField, { target: { name: "size", value: "" } })
+
+    expect(screen.getByText(/the name is required/i)).toBeInTheDocument()
+    expect(screen.getByText(/the size is required/i)).toBeInTheDocument()
+  })
+})
+
+describe("when the user submits the form", () => {
+  it("the submit button should be disabled until the request is done", async () => {
+    renderForm()
+
+    const submitButton = screen.getByRole("button", { name: /submit/i })
+    expect(submitButton).not.toBeDisabled()
+
+    user.click(submitButton)
+    expect(submitButton).toBeDisabled()
+
+    await waitFor(() => {
+      expect(submitButton).not.toBeDisabled()
+    })
+  })
 })
